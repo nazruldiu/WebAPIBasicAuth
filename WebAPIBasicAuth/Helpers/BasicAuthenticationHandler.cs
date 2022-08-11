04c8257c443f291e3a6554c70ee910ca1bb9d4d1
Nazrul Islam <nazrul.islam@technovista.com.bd>
2022-08-11 18:27:14 +0600

    initial commit

---
+++ WebAPIBasicAuth/Helpers/BasicAuthenticationHandler.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using WebAPIBasicAuth.Entities;
+using WebAPIBasicAuth.Services;
+
+namespace WebAPIBasicAuth.Helpers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        IUserService _userService;
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            IUserService userService) : base(options, logger, encoder, clock)
+        {
+            _userService = userService;
+        }
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            // skip authentication if endpoint has [AllowAnonymous] attribute
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null)
+                return AuthenticateResult.NoResult();
+
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+
+            User user = null;
+
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Parameter)).Split(':');
+                var username = credentials.FirstOrDefault();
+                var password = credentials.LastOrDefault();
+
+                user = await _userService.Authenticate(username, password);
+            }
+            catch (Exception ex)
+            {
+                return AuthenticateResult.Fail($"Authentication failed: {ex.Message}");
+            }
+
+            if (user == null)
+                return AuthenticateResult.Fail("Invalid Username or Password");
+
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+}
---
+++ WebAPIBasicAuth/Services/IUserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIBasicAuth.Entities;
+
+namespace WebAPIBasicAuth.Services
+{
+    public interface IUserService
+    {
+        Task<User> Authenticate(string username, string password);
+    }
+
+    public class UserService : IUserService
+    {
+        private List<User> userList = new List<User>
+        {
+            new User{Id = 1, Username="admin", Password="123456"},
+            new User{Id = 2, Username="user", Password="123456"}
+        };
+
+        public Task<User> Authenticate(string username, string password)
+        {
+            var user = Task.Run(()=> userList.SingleOrDefault(x => x.Username == username && x.Password == password));
+            return user;
+        }
+    }
+}
